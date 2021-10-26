379a148c9fae29890d65583888ee8dac3d80d6f0
Brett A C Sheffield <bacs@librecast.net>
2021-10-26 20:10:31 +0200

    fix Key extraction properties

--- src/key.js
+++ src/key.js
@@ -5,28 +5,28 @@ class Key {
 	get combo() {
 		return this.comboKeyHex;
 	}
-	get crypt() {
-		if (this.cryptKey === undefined) {
-			this.cryptKey = this.comboKeyHex.slice(0, sodium.crypto_box_PUBLICKEYBYTES * 2);
-		}
-		return this.cryptKey;
-	}
-	get sign() {
-		if (this.signKey === undefined) {
-			this.signKey = this.comboKeyHex.slice(sodium.crypto_box_PUBLICKEYBYTES * 2);
-		}
-		return this.signKey;
-	}
 	get cryptHex() {
 		if (this.cryptKeyHex === undefined) {
-			this.cryptKeyHex = sodium.from_hex(this.crypt);
+			this.cryptKeyHex = this.comboKeyHex.slice(0, sodium.crypto_box_PUBLICKEYBYTES * 2);
 		}
 		return this.cryptKeyHex;
 	}
 	get signHex() {
 		if (this.signKeyHex === undefined) {
-			this.signKeyHex = sodium.from_hex(this.sign);
+			this.signKeyHex = this.comboKeyHex.slice(sodium.crypto_box_PUBLICKEYBYTES * 2);
 		}
 		return this.signKeyHex;
+	}
+	get crypt() {
+		if (this.cryptKey === undefined) {
+			this.cryptKey = sodium.from_hex(this.cryptHex);
+		}
+		return this.cryptKey;
+	}
+	get sign() {
+		if (this.signKey === undefined) {
+			this.signKey = sodium.from_hex(this.signHex);
+		}
+		return this.signKey;
 	}
 }
