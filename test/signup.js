371b045954877644e3b4c4fe7879560c19ef5db0
Brett A C Sheffield <bacs@librecast.net>
2021-10-28 16:05:31 +0200

    signup test: reject bad email address

--- test/signup.js
+++ test/signup.js
@@ -2,15 +2,19 @@ QUnit.module('account signup', function() {
 	QUnit.test('new user account - bad email address', function(assert) {
 		console.log("== TEST: new user account - bad email address");
 		assert.timeout(2000);
-		const done = assert.async(2);
+		const done = assert.async(3);
 		sodium.ready.then(function () {
 			assert.ok(true, "sodium ready");
 			const kp = sodium.crypto_box_keypair();
 			const localpart = sodium.to_hex(sodium.randombytes_buf(16));
 			const invalidEmail = "@live.librecast.net";
 			const password = sodium.to_hex(sodium.randombytes_buf(16));
-			const auth = new Auth(kp, () => {
-				console.log("auth message received on reply channel");
+			const auth = new Auth(kp, (opcode, flags, fields, pre) => {
+				const view = new DataView(pre);
+				const responseCode = view.getUint8(0).toString();
+				assert.ok(opcode === 0x1, "opcode=" + opcode);
+				assert.ok(responseCode === "1", "signup rejected");
+				done();
 			});
 			auth.ready.then( () => {
 				assert.ok(true, "auth ready");
